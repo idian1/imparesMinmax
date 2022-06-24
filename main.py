3ceb24afeb92cfa6106672bf28be80c9c2f99e8b
idian1 <108105770+idian1@users.noreply.github.com>
2022-06-24 00:12:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ for i in range(num1, num2+1):
 
 print(f"la suma de todos los numeros impares entre {num1} y {num2} es de {suma}")
 
-# En este codigo el usuario ingresa un numero el que entra en un ciclo "for" el cual recorre todos los numeros desde
-# el numero minimo ingresado (num1) hasta el numero maximo ingresado (num2) y utilizando el operador modulo
+# En este codigo el usuario ingresa un numero minimo y un numero maximo los que entran en un ciclo "for" el cual recorre todos los numeros desde
+# el numero minimo ingresado (num1) hasta el numero maximo ingresado mas 1 (num2+1) y utilizando el operador modulo
 # comprueba si el numero es par o impar, si este es impar sera sumado a la variable suma,
-# luego de terminar el ciclo imprime la suma total de impares
-# En este codigo utilice el ciclo for por que era conveniente para la situacion requerida.
+# luego de terminar el ciclo imprime la suma total de los impares.
